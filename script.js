1931f003503e056a4a2bc68110160590deb2dd01
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-29 07:24:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,12 @@
 //your JS code here. If required.
-const audio = document.createElement("audio");
-function playAudio(btnRef){
-	let currentBtnId = btnRef.id;
-	switch(currentBtnId){
-			case "applause":
-			console.log("applauseBtnClicked");
-			audio.src = "./sounds/applause.mp4";
-			audio.play();
-			break;
-			
-			case "boo":
-			audio.src = "./sounds/boo.mp4";
-			audio.play();
-			break;
-			
-			case "gasp":
-			audio.src = "./sounds/gasp.mp4";
-			audio.play();
-			break;
-			
-			case "tada":
-			audio.src = "./sounds/tada.mp4";
-			audio.play();
-			break;
-			
-			case "victory":
-			audio.src = "./sounds/victory.mp4";
-			audio.play();
-			break;
-			
-			case "wrong":
-			audio.src = "./sounds/wrong.mp4";
-			audio.play();
-			break;
-			
-			case "stop":
-			audio.pause();
-			break;
-	}
+
+const audioElement = document.createElement("audio");
+
+function playSound(soundFile) {
+    audioElement.src = `./sounds/${soundFile}`;
+    audioElement.play();
+}
+
+function stopSound() {
+    audioElement.pause();
 }
