23c7386a4540d5e1b42ae2ac533a924f468b1868
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-29 07:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 //your JS code here. If required.
-var section = document.getElementById("buttons")
-// alert(section)
-section.addEventListener('click', function(e) {
-    if(e.target.className === 'btn') {
-        var soundName = `./sounds/${e.target.innerHTML}.mp3`;		
-		const audio = new Audio(soundName);
-		audio.play();
-		// alert(soundName)
-		
-    }
-	// alert(e.target.className)
+function playAudio(audioId) {
+	const audio = new Audio(`/sounds/${audioId}.mp3`);
+	audio.play();
+}
+
+document.querySelectorAll(`.btn`).forEach(function (button) {
+	button.addEventListener("click", function () {
+		if (this.id === 'applause' || this.id === 'boo' || this.id === 'gasp' || this.id === 'tada' || this.id === 'victory' || this.id === 'wrong') {
+			playAudio(this.id);
+		} else if(this.classList.contains('stop')){
+			document.querySelectorAll('audio').forEach(function(audio) {
+                audio.pause();
+                audio.currentTime = 0;
+            });)
+		}
+	});
 });
