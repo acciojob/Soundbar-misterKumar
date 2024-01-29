535fc296f243fcc47591441b344a4ded63647ab7
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-29 07:11:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,13 @@
 //your JS code here. If required.
-let myaudio=new Audio();
-
-function applause(){
-	myaudio.src= "./sounds/sound1";
-	myaudio.currentTime=0;
-	myaudio.play();
-	
-}
-function boo() {
-	myaudio.src="./sounds/sound2";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-function gasp() {
-	myaudio.src="./sounds/sound3";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-function tada() {
-	myaudio.src="./sounds/sound4";
-	myaudio.currentTime=0;
-	myaudio.play();
-
-}
-function victory() {
-	myaudio.src="./sounds/sound5";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-function wrong() {
-	myaudio.src="./sounds/sound6";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-function stop() {
-	myaudio.src="./sounds/sound7";
-	myaudio.currentTime=0;
-	myaudio.play();
-}
-
-function stop() {
-	myaudio.pause();
-	
-}
+var section = document.getElementById("buttons")
+// alert(section)
+section.addEventListener('click', function(e) {
+    if(e.target.className === 'btn') {
+        var soundName = `./sounds/${e.target.innerHTML}.mp3`;		
+		const audio = new Audio(soundName);
+		audio.play();
+		// alert(soundName)
+		
+    }
+	// alert(e.target.className)
+});
